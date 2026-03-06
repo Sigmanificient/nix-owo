e6680359eead199629c73d9959502f61034279e0
Sigmanificient <edhyjox@gmail.com>
2026-03-06 04:03:44 +0100

    Add test script

--- flake.nix
+++ flake.nix
@@ -20,7 +20,16 @@
       default = pkgs.mkShell {
         hardeningDisable = ["fortify"];
 
-        env.FILTERPATH_SOURCE_TEST = pkgs.filterpath.src;
+        env = {
+          FILTERPATH_SOURCE_TEST = pkgs.filterpath.src;
+          # sha256-FOewYznmWOWH2TyNySVoa+spvH4QlXnjlko+/zFiNik=
+
+          CRITERION_SOURCE_TEST = pkgs.criterion.src;
+          # sha256-X4m/uCyanS7HLtf6GyK4XuaT5i+HQt1PZC7gd813IVQ=
+
+          QTILE_SOURCE_TEST = pkgs.python3Packages.qtile.src;
+          # sha256-PPyI+IGvHBQusVmU3D26VjYjLaa9+94KUqNwbQSzeaI=
+        };
 
         inputsFrom = [
           self.packages.${pkgs.stdenv.hostPlatform.system}.nix-hash

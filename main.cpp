306161d619bd9261dcc3ef923be2aae2cea81e43
Sigmanificient <edhyjox@gmail.com>
2026-03-06 04:10:09 +0100

    Extract SAdmpPath

--- main.cpp
+++ main.cpp
@@ -6,6 +6,70 @@
 
 using namespace nix;
 
+// Patched method of Source Accessor
+
+static void SAdumpContents(SourceAccessor& sa, const CanonPath & path, Sink & sink)
+{
+    sink << "contents";
+
+    auto s = sa.readFile(path);
+
+    // SourceAccessor::readFile(3 params)
+    uint64_t size = s.size();
+
+    sink << size;
+    sink(s);
+
+    writePadding(size, sink);
+}
+
+void SAdump(SourceAccessor& sa, const CanonPath & path, Sink & sink)
+{
+    auto st = sa.lstat(path);
+
+    sink << "(";
+
+    if (st.type == SourceAccessor::tRegular) {
+        sink << "type" << "regular";
+        if (st.isExecutable)
+            sink << "executable" << "";
+        SAdumpContents(sa, path, sink);
+    }
+
+    else if (st.type == SourceAccessor::tDirectory) {
+        sink << "type" << "directory";
+
+        /* If we're on a case-insensitive system like macOS, undo
+           the case hack applied by restorePath(). */
+        StringMap unhacked;
+        for (auto & i : sa.readDirectory(path))
+                unhacked.emplace(i.first, i.first);
+
+        for (auto & i : unhacked) {
+            sink << "entry" << "(" << "name" << i.first << "node";
+            SAdump(sa, path / i.second, sink);
+            sink << ")";
+        }
+    }
+
+    else if (st.type == SourceAccessor::tSymlink)
+        sink << "type" << "symlink" << "target" << sa.readLink(path);
+
+    else
+        throw Error("file '%s' has an unsupported type", path);
+
+    sink << ")";
+}
+
+static
+void SAdumpPath(SourceAccessor& sa, const CanonPath &path, Sink &sink)
+{
+    sink << "nix-archive-1";
+    SAdump(sa, path, sink);
+}
+
+// End
+
 static std::string hash_to_string(Hash hash)
 {
     std::string s = "sha256-";
@@ -27,7 +91,7 @@ int main(int argc, char **argv)
     try {
         sink.writeUnbuffered(""); // ensure buffer exists (optional)
 
-        root.dumpPath(CanonPath{abspath.relative_path().string()}, sink);
+        SAdumpPath(root, CanonPath{abspath.relative_path().string()}, sink);
 
         HashResult result = sink.finish();
         std::cout << hash_to_string(result.hash) << "\n";
